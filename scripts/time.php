664ee9a3d0baef5618c008b7a34da6c1591d42da
Andreas Schroeder <an.schroeder@kostal.com>
2017-02-23 17:03:02 +0800

    Small improvement to time script.

--- scripts/time.php
+++ scripts/time.php
@@ -1,1 +1,11 @@
-<p>Current system time is <span style="font-weight:bold;"><?php echo strftime('%Y-%m-%d %T') ?></span>.</p>
+<?php /* -*- mode:html -*- */ ?>
+<p>
+  Current system date is
+  <span style="font-weight:bold;">
+    <?php echo strftime('%Y-%m-%d') ?>
+  </span>
+  and time is
+  <span style="font-weight:bold;">
+    <?php echo strftime('%T') ?>
+  </span>.
+</p>
