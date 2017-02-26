a6998e8a21de527206e97bb93f911a1f9d169fd2
Andreas Schroeder <andreas@a-netz.de>
2017-02-26 11:01:38 +0800

    Changed dhcp lease time to interval output instead of end time.

---
+++ utils/format.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Format output of a DateInterval value.
+ *
+ * If the value is NULL, $default is returned.
+ */
+function fmt_date_interval($ival, $default='') {
+    if($ival == NULL) {
+        return $default;
+    }
+
+    $fmt = '%h hours, %i minutes';
+    
+    if($ival->days > 0) {
+        $fmt = '%a days, ' . $fmt;
+    }
+    
+    return $ival->format($fmt);
+
+}
+
+/**
+ * Format output of a DateTime value.
+ *
+ * If the value is NULL, $default is returned.
+ */
+function fmt_date($dt, $default='') {
+    if($dt == NULL) {
+        return $default;
+    }
+
+    return $dt->format('%c');
+}
