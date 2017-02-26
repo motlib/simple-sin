1cd1971a5051a06e6e4d9c17efa7b383a50734ec
Andreas Schroeder <andreas@a-netz.de>
2017-02-26 15:05:16 +0800

    Added expand / collape indicator.

--- res/toggle.js
+++ res/toggle.js
@@ -1,12 +1,17 @@
 var toggle_content = function(el_h) {
-    var el_div = el_h.parentNode.getElementsByTagName('div')[0];
+    // heading id is head_script, so script name starts at char 5
+    var scriptname = el_h.id.substring(5);    
+    /* Get content area div tag */
+    var el_content = document.getElementById('content_' + scriptname);
+    /* Get toggle indicator span tag */
+    var el_tglind = document.getElementById('tglind_' + scriptname);
 
-    if(el_div.style.display == 'none')
-    {
-        el_div.style.display = 'block';
-    }
-    else
-    {
-        el_div.style.display = 'none';
+    if(el_content.style.display == 'none') {
+        el_content.style.display = 'block';
+        el_tglind.innerHTML = '[-]';
+    } else {
+        el_content.style.display = 'none';
+        el_tglind.innerHTML = '[+]';
+
     }
 }
--- tmpl/toolbox.php
+++ tmpl/toolbox.php
@@ -1,10 +1,10 @@
 <!-- -*- mode:html -*- -->
 
 <div class="toolbox">
-  <h2 class="heading" onclick="toggle_content(this);">
-    <?php echo $boxspec['title']; ?>
+  <h2 class="heading" id="head_<?= $script ?>" onclick="toggle_content(this);">
+    <span id="tglind_<?= $script ?>">[-]</span> <?php echo $boxspec['title']; ?>
   </h2>
-  <div class="content" id="<?php echo $script; ?>">
+  <div class="content" id="content_<?php echo $script; ?>">
     <?php echo $output; ?>
   </div>
 </div>
