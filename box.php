5e6045cd6526acfc8374b5eae9a6557adbfe4ee6
Andreas Schroeder <an.schroeder@kostal.com>
2017-03-03 10:28:47 +0800

    Added security checks to box.php for script parameter.

--- box.php
+++ box.php
@@ -2,9 +2,29 @@
 include_once 'utils/sin.php';
 include_once 'utils/render.php';
 
-//TODO: Add error handling and escaping!
-$script = $_GET['script'];
+/* Check if we get the script parameter. */
+if(array_key_exists('script', $_GET)) {
+    $script = $_GET['script'];
 
-$sin = new Sin();
-$sin->render_box($script, false);
+    /* Check if the script name is ok. */
+    $res = preg_match('/^[a-zA-Z_]+$/', $script);
+    if($res === 1) {
+        /* As we will include and run php scripts from disk based on
+         * the script name, the script name is restricted to
+         * characters and underscore. For security, it is not possible
+         * to specify any relative paths. */
+        
+        $sin = new Sin();
+        $sin->render_box($script, false);
+        
+        return;
+    } else {
+        $err = "Invalid script name '$script'.";
+    }
+} else {
+    $err = "No script name specified.";
+}
 
+/* Invalid script name or no script name given. */
+http_response_code(404);
+print("ERROR 404: $err");
