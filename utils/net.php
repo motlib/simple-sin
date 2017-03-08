be32f490f1035bdea97e1ffa92d452a385ddd94a
Andreas Schroeder <andreas@a-netz.de>
2017-03-09 07:07:37 +0800

    Added comments.

--- utils/net.php
+++ utils/net.php
@@ -1,17 +1,27 @@
 <?php
 include_once 'utils/cmd.php';
 
+/**
+ * Get network device info.
+ *
+ * @param $dev Network device name, e.g. eth0.
+ *
+ * @returns Assoc. array with device info.
+ */
 function net_get_dev_info($dev) {
     $devinfo = array();
 
-    /*
-      ip sample output:
-      2: ens160    inet 10.180.2.190/24 brd 10.180.2.255 scope global ens160\       valid_lft forever preferred_lft forever
-    */
-
+    /* ip sample output:
+     * 2: ens160    inet 10.180.2.190/24 brd 10.180.2.255 scope global ens160\
+     * valid_lft forever preferred_lft forever
+     */
     $data = get_cmd_output("ip -o -4 address show ${dev}");
     
-    $res = preg_match('/inet ([.0-9]+)\/([0-9]+) brd ([.0-9]+)/s', $data, $matches);
+    $res = preg_match(
+        '/inet ([.0-9]+)\/([0-9]+) brd ([.0-9]+)/s',
+        $data,
+        $matches);
+    
     if($res === 1) {
         $devinfo['ipaddress'] = $matches[1];
         $devinfo['netsize'] = $matches[2];
@@ -22,11 +32,10 @@ function net_get_dev_info($dev) {
         $devinfo['broadcast'] = 'n/a';
     }
       
-
     $devinfo['hwaddress'] = trim(file_get_contents("/sys/class/net/${dev}/address"));
     $devinfo['rx_bytes'] = file_get_contents("/sys/class/net/${dev}/statistics/rx_bytes");
     $devinfo['tx_bytes'] = file_get_contents("/sys/class/net/${dev}/statistics/tx_bytes");
-        
+    
     return $devinfo;
 }
 
