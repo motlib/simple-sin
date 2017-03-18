37fb2b7919cca3bffeac6ab4d59a3ca5684fdf9c
Andreas Schroeder <andreas@a-netz.de>
2017-03-18 09:35:29 +0800

    Removed debug output.

--- scripts/hostapd.php
+++ scripts/hostapd.php
@@ -3,12 +3,7 @@
 include_once 'utils/hostapd.php';
 
 function sin_get_hostapd($sin, &$context) {
-
     $stations = hostapd_get_stations();
-
-    print('<pre>');
-    print_r($stations[0]);
-    print('</pre>');
 
     $context['title'] = 'Access Point Statistics';
     $context['num'] = count($stations);
